5613c7dd48fe927e2d3be7798b80a36f67ac303d
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-04-17 13:59:46 +0200

    [add] server.php-variable

--- script.js
+++ script.js
@@ -5,7 +5,21 @@ createApp({
   data(){
     return{
       title: 'Discs List',
+      // il server.php viene chiamato da JS
+      apiUrl: 'server.php',
     }
+  },
+  methods:{
+    // CHIAMATA ad api esposta da server.php
+    getApi(){
+      axios.get(this.apiUrl)
+      .then(result => {
+        console.log(result.data)
+      })
+    }
+  },
+  mounted(){
+    this.getApi()
   }
 
 }).mount('#app')
---
+++ server.php
@@ -0,0 +1,6 @@
+<?php
+// richiamo file json e lo legge come stringa e lo salvo in variabile
+$json_str = file_get_contents('dischi.json');
+var_dump($json_str);
+
+?>
